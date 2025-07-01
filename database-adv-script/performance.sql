3635b1cff93f0bfedd4280c77cd75c4aacb849e7
Pelumiii1 <adesokanpelumi14@gmail.com>
2025-07-01 18:28:05 +0100

    hot fix

--- database-adv-script/performance.sql
+++ database-adv-script/performance.sql
@@ -1,4 +1,6 @@
--- Initial Query: Retrieve all bookings with user, property, and payment details
+-- Analyze the query's performance
+EXPLAIN ANALYZE
+-- Retrieve all bookings with user, property, and payment details
 SELECT
     b.booking_id,
     b.start_date,
@@ -13,30 +15,13 @@ SELECT
     p.location,
     py.payment_id,
     py.amount,
-    py.payment_date
+    py.payment_date,
+    py.payment_method
 FROM
     bookings b
 JOIN
     users u ON b.user_id = u.user_id
 JOIN
     properties p ON b.property_id = p.property_id
-JOIN
-    payments py ON b.booking_id = py.booking_id;
-
--- Refactored Query: Retrieve specific booking details
-SELECT
-    b.booking_id,
-    b.start_date,
-    b.end_date,
-    u.first_name,
-    u.last_name,
-    p.name AS property_name,
-    py.amount
-FROM
-    bookings b
-JOIN
-    users u ON b.user_id = u.user_id
-JOIN
-    properties p ON b.property_id = p.property_id
-JOIN
+LEFT JOIN
     payments py ON b.booking_id = py.booking_id;
